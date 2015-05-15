109416acf4763c5ea6d2b8df78da68ec0184b3d7
Ian Renfro <ian@renfro.org>
2015-05-14 18:07:27 -0700

    Making the Bird Fall
    Added the start Button
    set the bird to start at the top center of screen
    Uses Interpolator to make the bird fall
             we will probably change this to using Timeline instead of Interpolator

--- FlappyWorld/src/game/Main.java
+++ FlappyWorld/src/game/Main.java
@@ -1,10 +1,14 @@
 package game;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 
@@ -16,6 +20,23 @@ public class Main extends Application{
 	private Node flappy = null;
 	
     private void addActionEventHandler(){
+    	button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	//TODO: start the drop animation of the bird
+            	TranslateTransition translateTransition = new TranslateTransition(Duration.millis(2000), flappy);
+            	translateTransition.setFromY(flappy.getTranslateY());
+        	translateTransition.setToY(300);
+        	translateTransition.setInterpolator(new Interpolator() {
+    			protected double curve(double t) {
+    				double factor = .98;
+    				return factor * t * t + (1-factor)*t ;
+  			}
+                  });
+        	translateTransition.play();
+            	
+            }
+        });
     }
     
     private void addMouseEventHandler(){
@@ -30,17 +51,21 @@ public class Main extends Application{
 		
 		//TODO 2: add Flappy
 		ImageView flappy = new ImageView("flappy.png");
-		
+		flappy.preserveRatioProperty().set(true);
+		flappy.layoutXProperty().set(150);
+		flappy.layoutYProperty().set(50);
 		
 		
 		//TODO 3: add Button
-
+		button = new Button("Start");
+        	button.layoutXProperty().set(150);
 		
 		
 		//Create a Group 
 		root = new Group( );
 		root.getChildren().add(bkgrd );
 		root.getChildren().add(flappy);
+		root.getChildren().add(button);
 		
 		//TODO 4: add action handler to the button
 		addActionEventHandler();
