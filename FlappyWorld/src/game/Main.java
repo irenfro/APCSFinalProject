8bd281cda367132e668b0698be8b00ed48244796
Rebecca Wong <rebeccawong_604@yahoo.com>
2015-05-24 22:55:53 -0700

    -added word displays for start & end game -added threshold for checkLocation to check every loop of interpolator for endGame -changed to no maximum height, just goes off screen like in original game

--- FlappyWorld/src/game/Main.java
+++ FlappyWorld/src/game/Main.java
@@ -5,11 +5,11 @@ import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+//import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -17,15 +17,21 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 
-	private Button button = null;
-	private Group root = null;
-	private ImageView bkgrd = null ;
-	private ImageView flappy = null;
+//	private Button button = null;
+	static private Group root = null;
+	static private ImageView bkgrd = null ;
+	static private ImageView flappy = null;
+	static private ImageView ground = null;
+	static private ImageView clickRun = null;
+	static private ImageView instructions = null;
+	static private ImageView getReady = null;
+	static private ImageView gameOver=null;
 
 	static final double g = 300, boostV = -150, sceneWidth=400, sceneHeight=400;
 	static final double start_x = 150, start_y = 150; // starting y position
 	static final double max_y = sceneHeight*0.9-23; // end position
 	static double v, duration, range;
+	static TranslateTransition movingGround;
 	static Timeline timeline;
 	static Interpolator interpolator;
 	static boolean endGame;
@@ -34,68 +40,88 @@ public class Main extends Application {
 		return ((-velocity+Math.sqrt(velocity*velocity+(2*g*distance)))/g);
 	}
 	private void checkLocation(){
-		if(flappy.getY()>=max_y){ // end game if hits bottom
+		double threshold=5;
+		if(flappy.getY()>=max_y-threshold){ // end game if hits bottom 
 			endGame=true;
+			movingGround.stop();
+			timeline.stop();
+			root.getChildren().add(gameOver);
 		}
 	}
 	private void interpolator(){
 		interpolator = new Interpolator(){
 			@Override
 			protected double curve (double t){
+//				if (flappy.getY()<=10){ //if hits top, go to free fall
+//					range=max_y-flappy.getY();
+//					v=0;
+//					duration = calcTime(range,v);
+//				}
+				double time =  t * duration;
+				int distance = (int) ((v*time)+(0.5*g*time*time));	
+				System.out.println(flappy.getY());
 				checkLocation();
-				 if (flappy.getY()<=10){ //if hits top, go to free fall
-					range=max_y-flappy.getY();
-					v=0;
-					duration = calcTime(range,v);
-				}
-				double time = t * duration;
-				double distance = (v*time)+(0.5*g*time*time);	
-				
 				return distance/range;
 			}
 		};
 	}
-	
+
 	private void addMouseEventHandler(){
 		root.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
+			int n=0;
 			@Override
 			public void handle(MouseEvent event) {
-				checkLocation();
-				if(timeline!=null){
-					timeline.stop();
+				n++;
+				if(n==1){
+					moveGround(); 
+					root.getChildren().remove(clickRun);					
+					root.getChildren().add(instructions);
+					root.getChildren().add(getReady);
 				}
-				if(!endGame){
-				range = max_y-flappy.getY();
-				v=boostV;	
-				duration = calcTime(range,boostV);
-				timeline = new Timeline();
-				KeyValue kv = new KeyValue(flappy.yProperty(),max_y, interpolator);
-				final KeyFrame kf = new KeyFrame(Duration.millis(duration * 1000), kv);	
-				timeline.getKeyFrames().add(kf);
-				timeline.play();
+				else{
+					root.getChildren().remove(instructions);
+					root.getChildren().remove(getReady);
+					if(!endGame){
+						if(timeline!=null){
+							timeline.stop();
+						}
+						range = max_y-flappy.getY();
+						v=boostV;	
+						duration = calcTime(range,boostV);
+						timeline = new Timeline();
+						KeyValue kv = new KeyValue(flappy.yProperty(),max_y, interpolator);
+						final KeyFrame kf = new KeyFrame(Duration.millis(duration * 1000), kv);	
+						timeline.getKeyFrames().add(kf);
+//						checkLocation();
+						timeline.play();
+					}
 				}
 			}
 		});
 	}
 
-	private ImageView movingGround(double x){
-		ImageView ground = new ImageView("ground.png");
-		ground.setLayoutX(0);
-		ground.setLayoutY(sceneHeight*0.9);
-		ground.setFitWidth(sceneWidth*2);
-		TranslateTransition transTransition = new TranslateTransition(new Duration(2500), ground);
-		transTransition.setToX(-400);
-		transTransition.setInterpolator(Interpolator.LINEAR);
-		transTransition.setCycleCount(Timeline.INDEFINITE);
-		transTransition.play();
+	private ImageView moveGround(){
+		movingGround = new TranslateTransition(new Duration(2000), ground);
+		movingGround.setToX(-sceneWidth);
+		movingGround.setInterpolator(Interpolator.LINEAR);
+		movingGround.setCycleCount(Timeline.INDEFINITE);
+		movingGround.play();
 		return ground;
 	}
+
+	//	private ImageView moveObstacles(){
+	//		
+	//	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		//TODO 1: add background
-		ImageView ground = movingGround(0);
+		ground = new ImageView("ground.png");
+		ground.setLayoutX(0);
+		ground.setLayoutY(sceneHeight*0.9);
+		ground.setFitWidth(sceneWidth*2);
+
 		bkgrd = new ImageView("background2.png");
 
 		//TODO 2: add Flappy
@@ -104,18 +130,34 @@ public class Main extends Application {
 		flappy.xProperty().set(start_x);
 		flappy.yProperty().set(start_y);
 
+
+		//initial displays
+		clickRun= new ImageView("clickrun.png");
+		clickRun.setLayoutX(sceneWidth/6);
+		clickRun.setLayoutY(sceneHeight/2);
+		getReady = new ImageView("getready.png");
+		getReady.setLayoutX(sceneWidth/4);
+		getReady.setLayoutY(sceneHeight/6);
+		instructions = new ImageView("instructions.png");
+		instructions.setLayoutX(sceneWidth/3.5);
+		instructions.setLayoutY(sceneHeight/2);
+		gameOver = new ImageView("gameover.png");
+		gameOver.setLayoutX(sceneWidth/4);
+		gameOver.setLayoutY(sceneHeight/4);
+
 		//Create a Group 
 		root = new Group( );
 		root.getChildren().add(bkgrd );
 		root.getChildren().add(ground );
 		root.getChildren().add(flappy);
+		root.getChildren().add(clickRun);
 
+		System.out.println(max_y);
 		//TODO 4: add action handler to the button
 		addMouseEventHandler();
 		interpolator();
-		
-		//TODO 5: add mouse handler to the scene
 
+		//TODO 5: add mouse handler to the scene
 
 		//Create scene and add to stage
 		Scene scene = new Scene(root, sceneWidth, sceneHeight);
